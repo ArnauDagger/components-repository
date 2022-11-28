b51a2579272c96e69a923fbba982fa344d95dac3
Arnau <arnaudagger@gmail.com>
2022-11-28 11:21:11 +0100

    Initial commit

---
+++ components.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * Dashboard. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * this starts the plugin.
+ *
+ * @link              https://recambios.triginer.com/
+ * @since             1.0.0
+ * @package           Components
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Components Plugin
+ * Plugin URI:        https://recambios.triginer.com/
+ * Description:       Plugin to automatically create a table in the description of certain products
+ * Version:           1.0.0
+ * Author:            Arnau
+ * Author URI:        arnaudagger@gmail.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       components
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-components-activator.php';
+
+/**
+ * The code that runs during plugin deactivation.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-components-deactivator.php';
+
+/** This action is documented in includes/class-plugin-name-activator.php */
+register_activation_hook( __FILE__, array( 'Components_Activator', 'activate' ) );
+
+/** This action is documented in includes/class-plugin-name-deactivator.php */
+register_activation_hook( __FILE__, array( 'Components_Deactivator', 'deactivate' ) );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * dashboard-specific hooks, and public-facing site hooks.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-components.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_components() {
+
+	$plugin = new Components();
+	$plugin->run();
+
+}
+run_components();
---
+++ includes/class-components-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Components
+ * @subpackage Components/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Components
+ * @subpackage Components/includes
+ * @author     Your Name <arnaudagger@gmail.com>
+ */
+class Components_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
