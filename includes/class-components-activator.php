4d0da029cd6c93a0d2c49cf70f1a74b5c8d5c33b
Arnau <arnaudagger@gmail.com>
2022-11-28 12:13:31 +0100

    First commit
    First commit adding create_components_table() function inside activator class

--- includes/class-components-activator.php
+++ includes/class-components-activator.php
@@ -30,7 +30,25 @@ class Components_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		create_components_table();
 	}
+	function create_components_table(){
+        global $wpdb;
+        $charset_collate = $wpdb->get_charset_collate();
+        $table_name = $wpdb->prefix . 'components';
+        $sql="CREATE TABLE Zi6w2ai2T_components (
+            meta_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+            parent_id BIGINT(20) NOT NULL DEFAULT '0',
+            component_id BIGINT(20) NOT NULL DEFAULT '0',
+            item INT,
+            component_name LONGTEXT COLLATE utf8mb4_unicode_520_ci,
+            quantity INT,
+            component_sku LONGTEXT COLLATE utf8mb4_unicode_520_ci,
+            PRIMARY KEY (meta_id)
+        );";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta( $sql );
+    }
 
 }
